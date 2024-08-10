5e84f3008789d5fc52a95534c77cd8bd81729b27
ru <ru@fufu.moe>
2024-08-11 02:45:58 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
     ];
 
+  virtualisation.virtualbox.guest.enable = true;
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
