3d96559be8006a8fb150076b0d0d28e5c3b64bbd
ru <ru@fufu.moe>
2024-08-11 05:06:58 +0800

    .

--- docker.nix
+++ docker.nix
@@ -4,7 +4,8 @@
   virtualisation.docker = {
     enable = true;
     daemon.settings = {
-      ipv6 = true;
+      log-driver = "json-file";
+      log-opts = { max-size = "100M"; };
       registry-mirrors = [ "https://docker.m.daocloud.io" ];
     };
     storageDriver = "btrfs";
