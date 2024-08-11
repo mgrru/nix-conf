bc7dc1d9cee5b927d232b1c3fe22edc3797d32d1
ru <ru@fufu.moe>
2024-08-11 17:55:07 +0800

    add a pkg

--- user-conf.nix
+++ user-conf.nix
@@ -11,6 +11,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGDMgOItE9fReXLvuhCawIzjuy2fqTghEJQxINMdkEE ru@fufu.moe"
     ];
     packages = with pkgs; [
+      tree
       neofetch
       nixfmt-classic
       nil
