107d9bf33a7a42e97263696c2daadf50b5dfd482
ru <ru@fufu.moe>
2024-08-15 22:14:07 +0800

    add substituters

--- flake.nix
+++ flake.nix
@@ -4,6 +4,13 @@
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      # nix community's cache server public key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
@@ -69,6 +76,12 @@
               # environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
             }
           )
+
+          {
+            # given the users in this list the right to specify additional substituters via:
+            #    1. `nixConfig.substituters` in `flake.nix`
+            nix.settings.trusted-users = [ "ru" ];
+          }
         ];
       };
     };
