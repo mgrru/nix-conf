ca84ff7c7bd62aa6c925440187b9ee6dbc7f0aed
ru <ru@fufu.moe>
2024-08-15 21:21:04 +0800

    upd net conf

--- services/v2raya.nix
+++ services/v2raya.nix
@@ -2,6 +2,18 @@
 
 {
   services.v2raya.enable = true;
-  services.v2ray.enable = true;
+  services.v2ray = {
+    enable = true;
+    config = {
+      inbounds = [
+        {
+          listen = "127.0.0.1";
+          port = 1080;
+          protocol = "http";
+        }
+      ];
+      outbounds = [ { protocol = "freedom"; } ];
+    };
+  };
   services.prometheus.exporters.v2ray.openFirewall = true;
 }
