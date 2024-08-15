bb88b50b6841664e8936b8f3636efc85765fc761
ru <ru@fufu.moe>
2024-08-15 16:09:36 +0800

    close firewall

--- sys-conf/configuration.nix
+++ sys-conf/configuration.nix
@@ -94,7 +94,7 @@
   # networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ 22 ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
