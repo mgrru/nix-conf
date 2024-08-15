59faa5b4849e9b788417756c7880ed7d87a96277
ru <ru@fufu.moe>
2024-08-15 18:08:17 +0800

    upd ssh conf

--- services/ssh.nix
+++ services/ssh.nix
@@ -6,6 +6,17 @@
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
+      X11Forwarding = true;
     };
+  };
+
+  services.sshd.enable = true;
+  programs.ssh = {
+    startAgent = true;
+    forwardX11 = true;
+    hostKeyAlgorithms = [
+      "ssh-ed25519"
+      "ssh-rsa"
+    ];
   };
 }
