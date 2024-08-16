003a4ea400491349cb8b2f5aabaf6a9dc59dcd76
ru <ru@fufu.moe>
2024-08-16 18:37:59 +0800

    upd bash conf

--- programs/bash.nix
+++ programs/bash.nix
@@ -18,10 +18,11 @@
   };
   programs.bash.promptInit = ''
     clear_color="\e[0m"
+    git_branch_name=""
     get_os_name() {
       os_name=$(uname -a)
       left_str="\e[0m\e[34;40;1m "
-      right_str=" \e[0m"$clear_color
+      right_str=$clear_color
       if [[ $os_name =~ "Arch" ]]
       then
         echo $left_str" "$right_str
@@ -38,30 +39,38 @@
         echo $left_str" "$right_str
       fi
     }
+    get_user_name() {
+      left_str="\e[37;40;7m$clear_color\e[34;47;1m "
+      right_str=" "$clear_color
+
+      user_str="\e[31;47m\u"
+      at_str="\e[31;47;1m@"
+      hostname_str="\e[34;47m\h"
+
+      echo $left_str$user_str$at_str$hostname_str$right_str
+    }
     get_git_branch() {
       git_branch_name=$(git branch --show-current 2>/dev/null)
-      left_str="\e[34;7m\e[0m\e[37;44;1m "
-      right_str=" \e[0m\[\e[34m\]"$clear_color
+      left_str="\e[34;47;7m\e[0m\e[33;44m "
+      right_str=" "$clear_color
       if [ $git_branch_name ]
       then
         echo $left_str" "$git_branch_name$right_str
       fi
     }
     get_workspace() {
-      left_str="\e[32;7m\e[0m\e[37;42;1m "
-      right_str=" \e[0m\[\e[32m\]"$clear_color
-      echo $left_str"\w"$right_str
+      git_branch_name=$(git branch --show-current 2>/dev/null)
+      if [ -z $git_branch_name ]
+      then
+        left_str="\e[31;47;7m\e[0m\e[37;41m "
+        right_str=" \e[0m\[\e[31m\]"$clear_color
+        echo $left_str"\w"$right_str
+      else
+        left_str="\e[31;44;7m\e[0m\e[37;41m "
+        right_str=" \e[0m\[\e[31m\]"$clear_color
+        echo $left_str"\w"$right_str
+      fi
     }
-    get_user_name() {
-      left_str="\e[36;40;7m$clear_color\e[34;46;1m "
-      right_str=" $clear_color\e[36m"$clear_color
-
-      user_str="\e[35m\u"
-      at_str="\e[37m@"
-      hostname_str="\e[31m\h"
-
-      echo $left_str$user_str$at_str$hostname_str$right_str
-    }
-    PROMPT_COMMAND='PS1="$(get_os_name)$(get_user_name)$(get_git_branch)$(get_workspace) \e[37;2m=> 󰅒 \t =>  \s:\v$clear_color\n>"'
+    PROMPT_COMMAND='PS1="$(get_os_name)$(get_user_name)$(get_git_branch)$(get_workspace) \e[37;2m 󰅒 \t   \s:\v$clear_color\n>"'
   '';
 }
