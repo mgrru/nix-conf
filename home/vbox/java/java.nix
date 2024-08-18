13b17ad05e7caea9896e6743989e64f00c776b62
ru <ru@fufu.moe>
2024-08-19 02:13:05 +0800

    fix error

--- home/vbox/java/java.nix
+++ home/vbox/java/java.nix
@@ -4,10 +4,10 @@ let
   java_version = 21;
 in
 {
-  nixpkgs.overlays.default = final: prev: rec {
+  nixpkgs.overlays.default = (final: prev: rec {
     jdk = prev."jdk${toString java_version}";
     maven = prev.maven.override { jre = jdk; };
-  };
+  });
 
   home.packages = with pkgs; [ maven ];
 
