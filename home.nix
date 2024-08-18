8bb68d82d965e92553c1774b7d0a330f6c85bf75
ru <ru@fufu.moe>
2024-08-18 19:28:35 +0800

    upd

--- home.nix
+++ home.nix
@@ -24,9 +24,9 @@
   # 通过 home.packages 安装一些常用的软件
   # 这些软件将仅在当前用户下可用，不会影响系统级别的配置
   # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
-  # home.packages = with pkgs;[
-
-  # ];
+  home.packages = with pkgs;[
+    kitty
+  ];
 
   programs.neovim = {
     enable = true;
