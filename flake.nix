03063081a45b3b3a30ccd3bbbaa78006510821ea
ru <ru@fufu.moe>
2024-08-20 01:09:04 +0800

    upd

--- flake.nix
+++ flake.nix
@@ -77,7 +77,7 @@
               pkgs = import trdpkgs { };
             in
             {
-              trdpkgs.overlays.default = final: prev: rec {
+              pkgs.overlays.default = final: prev: rec {
                 jdk = prev."jdk${toString java_version}";
                 maven = prev.maven.override { jre = jdk; };
               };
