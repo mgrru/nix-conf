fd981e1e8165d55ee1ceb736fe97b6d3f1b82842
ru <ru@fufu.moe>
2024-08-20 20:25:19 +0800

    upd

--- sys-conf/network.nix
+++ sys-conf/network.nix
@@ -4,13 +4,13 @@
   networking = {
     hostName = "rnix"; # Define your hostname.
     networkmanager.enable = true;
-    firewall = {
-      allowedTCPPorts = [ 22 443 6000 ];
-      allowedUDPPorts = [ 22 443 6000 ];
-    };
+    # firewall = {
+    #   allowedTCPPorts = [ 22 443 6000 ];
+    #   allowedUDPPorts = [ 22 443 6000 ];
+    # };
   };
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 }
